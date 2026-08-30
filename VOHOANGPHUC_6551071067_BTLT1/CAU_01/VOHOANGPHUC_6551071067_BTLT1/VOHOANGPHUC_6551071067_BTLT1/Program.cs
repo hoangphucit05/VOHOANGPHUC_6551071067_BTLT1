c62c0f2704728d8cd2ee8da66bd84373c8e0f8a9
hoangphucit05 <hoangphuc.it05@gmail.com>
2026-08-31 04:06:56 +0700

    Add files via upload

---
+++ VOHOANGPHUC_6551071067_BTLT1/CAU_01/VOHOANGPHUC_6551071067_BTLT1/VOHOANGPHUC_6551071067_BTLT1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string hoTen;
+        int tuoi;
+        double chieuCao;
+        float canNang;
+        bool daTotNghiep;
+
+        Console.Write("Nhap ho ten: ");
+        hoTen = Console.ReadLine();
+        Console.Write("Nhap tuoi: ");
+        tuoi = int.Parse(Console.ReadLine());
+        Console.Write("Nhap chieu cao (m): ");
+        chieuCao = double.Parse(Console.ReadLine());
+        Console.Write("Nhap can nang (kg): ");
+        canNang = float.Parse(Console.ReadLine());
+        Console.Write("Da tot nghiep (True/False): ");
+        daTotNghiep = bool.Parse(Console.ReadLine());
+        Console.WriteLine("Ho ten: " + hoTen);
+        Console.WriteLine("Tuoi: " + tuoi);
+        Console.WriteLine("Chieu cao: " + chieuCao + " m");
+        Console.WriteLine("Can nang: " + canNang + " kg");
+        Console.WriteLine("Da tot nghiep: " + daTotNghiep);
+    }
+}
---
+++ VOHOANGPHUC_6551071067_BTLT1/CAU_02/VOHOANGPHUC_6551071067_BTLT1/VOHOANGPHUC_6551071067_BTLT1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("MSSV: 6551071067");
+
+        double a, b;
+
+        Console.Write("Nhap a: ");
+        a = double.Parse(Console.ReadLine());
+        Console.Write("Nhap b: ");
+        b = double.Parse(Console.ReadLine());
+        Console.WriteLine("Cong: " + (a + b));
+        Console.WriteLine("Tru: " + (a - b));
+        Console.WriteLine("Nhan: " + (a * b));
+        Console.WriteLine("Chia: " + (a / b));
+        Console.WriteLine("Chia lay phan nguyen: " + ((int)a / (int)b));
+        Console.WriteLine("Chia lay phan du: " + ((int)a % (int)b));
+        Console.WriteLine("Luy thua: " + Math.Pow(a, b));
+    }
+}
